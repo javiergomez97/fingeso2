c2d88a7658d8f4b2367d29344389ef7130281dc6
Daniel <daniel.osorio.w@usach.cl>
2021-03-04 18:58:17 -0300

    vuetify home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="home">
+  
+  <div>
+  
     <img alt="Symbiose logo" src="../assets/symbiose-logo-72.png">
     <HelloWorld msg="Symbiose Projects Managment"/>
   </div>
@@ -7,18 +9,9 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  metaInfo: {
-      // if no subcomponents specify a metaInfo.title, this title will be used
-      title: 'Default Title',
-      // all titles will be injected into this template
-      titleTemplate: '%s | My Awesome Webapp'
-    },
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
